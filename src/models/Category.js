ba754e2531efb7d834d2a5c345460d7eb9f8c5e9
Sachin V S <vs1sachi12@gmail.com>
2026-05-02 11:06:47 +0530

    category.js-model

---
+++ src/models/Category.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+module.exports = mongoose.model("Category", categorySchema);
