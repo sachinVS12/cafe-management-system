22c05864133c044a5e1ecde4a85dd161122f5913
Sachin V S <vs1sachi12@gmail.com>
2026-05-07 12:41:27 +0530

    databse config fiel

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      process.env.MONGODB_URI || "mongodb://localhost:27017/cafe_management",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+    );
     console.log("MongoDB Connected Successfully");
   } catch (error) {
     console.error("MongoDB Connection Error:", error);
